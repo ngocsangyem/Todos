1c5aa49897b3817f07aa4ef9ebbd8e93fcb1f9e5
Nguyen Sang <nnsang24@gmail.com>
2019-06-05 16:05:21 +0700

    add toggle

--- src/app/typescript/service/todo.service.ts
+++ src/app/typescript/service/todo.service.ts
@@ -18,7 +18,7 @@ export class TodoService implements TodoServiceInterface {
 		for (let i = 0; i < length; i += 1) {
 			ID += chars.charAt(Math.floor(Math.random() * chars.length));
 		}
-		return ID;
+		return 'task-' + ID;
 	}
 	private addTask(task: string): Todo {
 		let todo: Todo = {
@@ -32,30 +32,26 @@ export class TodoService implements TodoServiceInterface {
 		return todo;
 	}
 
-	private renderTodo(todoItem: {id: string, name: string, state: boolean}): void{
+	private renderTodo(task: {id: string, name: string, state: boolean}): void{
 		let container = <HTMLElement>document.getElementById('bodyTasks');
 		let html: string, newHtml: string, htmlDom;
-		html =  `<li class="td__body-task${todoItem.state === true ? ' td__body-task--complete' : '' }" data-id="${todoItem.id}" data-state="${todoItem.state === true ? 'complete' : 'active'}">
+		html =  `<li class="td__body-task${task.state === true ? ' td__body-task--complete' : '' }" data-id="${task.id}" data-state="${task.state === true ? 'complete' : 'active'}">
 					<input class="td__body-task--toggle" type="checkbox">
-					<span>${todoItem.name}</span>
-					<button class="td__body-task--destroy" data-id="${todoItem.id}"><i class="mdi mdi-window-close"></i></button>
+					<span>${task.name}</span>
+					<button class="td__body-task--destroy" data-id="${task.id}"><i class="mdi mdi-window-close"></i></button>
 				</li>`
 		
 		container.insertAdjacentHTML("beforeend", html);
 
-		// let findId = <HTMLElement>document.querySelector(`[data-id="${todoItem.id}"]`)
+		let findId = <HTMLElement>document.querySelector(`[data-id="${task.id}"]`)
 
-		// if (findId) {
-		// 	findId.addEventListener('click', event => {
-		// 		this.toggleState(todoItem);
-		// 	})
-		// }
-		htmlDom = new DOMParser().parseFromString(html, 'text/xml')
-		// htmlDom = html.trim();
-		console.log(htmlDom.firstChild);
-		htmlDom.firstChild.addEventListener('click', function(){
-			console.log(123);
-		})
+		if (findId) {
+			findId.addEventListener('click', event => {
+				this.toggleState(task);
+				findId.remove()
+				this.renderTodo(task)
+			})
+		}
 		
 	}
 	
@@ -95,8 +91,16 @@ export class TodoService implements TodoServiceInterface {
 		return String(this.todos.length);
 	}
 
-	findTodoId (todoId: number){
-		
+	renderAfterClick (todo, container){
+		for (todo in this.todos) {
+			if (this.todos.hasOwnProperty(todo)) {
+				let element = this.todos[todo];
+				if (element.id) {
+					container.insertAdjacentHTML('beforeend', this.renderTodo(element))
+				}
+				
+			}
+		}
 	}
 	toggleState(todo: Todo): boolean{
 		todo.state = !todo.state
